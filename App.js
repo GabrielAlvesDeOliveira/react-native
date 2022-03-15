4dbc71f0b0d8b42bf0a55d1ee7488e8381709cdb
GabrielAlvesDeOliveira <alvesg2003@gmail.com>
2022-03-15 15:51:36 -0300

    Initilizing new project

--- App.js
+++ App.js
@@ -1,24 +1,16 @@
 import React, {Components} from 'react'
-import { StyleSheet, Text, View, Switch } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 export default class App extends Components{
   
   constructor(props){
     super(props)
-    this.state = { status: false}
+    this.state = {  }
   }
 
   render(){
     return (
       <View style={styles.container}>
 
-        <Switch
-          value={this.state.status}
-          onValueChange={(valorSwitch)=> this.setState({status: valorSwitch})}
-          thumbColor="#FF0000"
-          />
-          <Text>
-            {(this.state.status) ? "Ativo" : "Inativo"}
-          </Text>
       </View>
 
     );
