1f7e2410b5ab8619934e6c5a61cd7729aa859244
GabrielAlvesDeOliveira <alvesg2003@gmail.com>
2022-03-17 13:52:11 -0300

    useRef

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect, useMemo} from 'react'
+import React, {useState, useEffect, useMemo, useRef} from 'react'
 import { StyleSheet, Text, View, TouchableOpacity, TextInput} from 'react-native';
 import AsyncStorage from '@react-native-community/async-storage';
 
@@ -6,6 +6,7 @@ function App(){
   
   const [nome, setNome] = useState('')
   const [input, setInput] = useState('')
+  const nomeInput = useRef(null)
 
   useEffect(()=>{
     
@@ -35,12 +36,16 @@ function App(){
     setInput('')
   }
 
+  function novoNome(){
+    nomeInput.current.focus()
+  }
+
   const letrasNome = useMemo(()=> nome.length, [nome])
 
   return(
     <View style={styles.container}>
 
-      <TextInput placeholder='Seu nome' value={input} onChangeText={(text)=>setInput(text)}/>
+      <TextInput placeholder='Seu nome' value={input} onChangeText={(text)=>setInput(text)} ref={nomeInput}/>
 
       <TouchableOpacity style={styles.btn} onPress={alteraNome}>
         <Text style={styles.btnText}> Altera nome </Text>
@@ -48,6 +53,10 @@ function App(){
 
       <Text> {nome} </Text>
       <Text>  Tem {letrasNome} letras. </Text>
+
+      <TouchableOpacity onPress={novoNome}>
+        <Text>Novo nome</Text>
+      </TouchableOpacity>
 
     </View>
   ) 
