c86308d82f818e849b3a6b7bf327aa15f7f760e0
GabrielAlvesDeOliveira <alvesg2003@gmail.com>
2022-03-17 09:38:13 -0300

    Usando Interpolações

--- App.js
+++ App.js
@@ -5,58 +5,35 @@ export default class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      LarAnimada: new Animated.Value(150),
-      AltAnimada: new Animated.Value(50),
-      OpacidadeAnimada: new Animated.Value(0)
+
+      LarAni: new Animated.Value(0)
+
     }
 
-    this.carregarGrafico = this.carregarGrafico.bind(this)
-
-  }
-
-  carregarGrafico() {
-
-    Animated.sequence([
-
-      Animated.timing(
-        this.state.OpacidadeAnimada,
-        {
-          toValue: 1,
-          duration: 400
-        }
-      ),
-
-      Animated.timing(
-        this.state.AltAnimada,
-        {
-          toValue: 300,
-          duration: 1000
-        }
-      )
-    ]).start()
+    Animated.timing(
+      this.state.LarAni,
+      {
+        toValue: 100,
+        duration: 5000,
+        useNativeDriver: false
+      }
+    ).start()
 
   }
 
   render() {
 
+    let porcentagemAnimate = this.state.LarAni.interpolate({
+      inputRange: [0, 100],
+      outputRange: ['0%', '100%']
+    })
+
     return (
       <View style={styles.container}>
+        
+        <Animated.View style={{backgroundColor: '#4169E1', width: porcentagemAnimate, height: 25}}>
 
-        <View style={{ height: 80, alignItems: 'center', justifyContent: 'center', flexDirection: 'row', backgroundColor: '#4169E1', opacity: this.state.OpacidadeAnimada }}>
-          <TouchableOpacity onPress={this.carregarGrafico}>
-            <Text style={{ fontSize: 25, color: "#FFF" }}>
-              Gerar grafico
-            </Text>
-          </TouchableOpacity>
-        </View>
-
-        <View style={{ flex: 1, flexDirection: 'column', justifyContent: 'flex-end', alignItems: 'center' }}>
-
-
-          <Animated.View style={{ width: this.state.LarAnimada, height: this.state.AltAnimada, backgroundColor: '#FF0000', justifyContent: 'center'}}>
-            <Text style={{ color: '#FFF', fontSize: 25, textAlign: 'center' }}>Carregando...</Text>
-          </Animated.View>
-        </View>
+        </Animated.View>
 
       </View>
     );
@@ -68,7 +45,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
+    alignItems: 'flex-start',
     marginTop: 20
   }
 });
