85e388afb1cd313773c4ae72623bd8dd503c4ca1
GabrielAlvesDeOliveira <alvesg2003@gmail.com>
2022-03-18 09:43:51 -0300

    buscador de cep

--- App.js
+++ App.js
@@ -1,78 +1,115 @@
-import React, {useState, useEffect, useMemo, useRef} from 'react'
-import { StyleSheet, Text, View, TouchableOpacity, TextInput} from 'react-native';
-import AsyncStorage from '@react-native-community/async-storage';
+import axios from 'axios';
+import React, {useState, useRef} from 'react'
+import { StyleSheet, Text, View, TouchableOpacity, TextInput, SafeAreaView, KeyBoard} from 'react-native';
+import api from './src/services/api'
 
 function App(){
-  
-  const [nome, setNome] = useState('')
-  const [input, setInput] = useState('')
-  const nomeInput = useRef(null)
 
-  useEffect(()=>{
-    
-    async function getStorage(){
-      const nomeStorage = await AsyncStorage.getItem('nomes')
-      if(nomeStorage !== null){
-        setNome(nomeStorage)
-      }
+  const [cep, setCep] = useState('')
+  const inputRef = useRef(null)
+  const [cepUser, setCepUser] = (null)
+  async function buscar(){
+    if(cep == ''){
+      alert('Digite um cep valido')
+      setCep('')
+      return;
     }
 
-    getStorage()
-
-  },[])
-
-  useEffect(()=>{
-
-    async function saveStorage(){
-      await AsyncStorage.setItem('nomes', nome)
+    try {
+      const response = await axios.get(`/${cep}/json`)
+      setCepUser(response.data)
+      KeyBoard.dismiss()
+    } catch (error) {
+      console.log('ERROR: ', error);
     }
-
-    saveStorage()
-
-  },[nome])
-  
-  function alteraNome(){
-    setNome(input)
-    setInput('')
   }
 
-  function novoNome(){
-    nomeInput.current.focus()
+  function limpar(){
+    setCep('')
+    inputRef.current.focus()
   }
-
-  const letrasNome = useMemo(()=> nome.length, [nome])
 
   return(
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
+      <View style={{alignItems: 'center'}}>
+        <Text style={styles.text}> Digite o CEP desejado</Text>
+        <TextInput 
+        style={styles.input}
+        placeholder="EX: 00000000"
+        value={cep}
+        onChangeText={(text) => setCep(text)}
+        keyboardType="numeric"
+        ref={inputRef}
+        />
+      </View>
 
-      <TextInput placeholder='Seu nome' value={input} onChangeText={(text)=>setInput(text)} ref={nomeInput}/>
-
-      <TouchableOpacity style={styles.btn} onPress={alteraNome}>
-        <Text style={styles.btnText}> Altera nome </Text>
-      </TouchableOpacity>
-
-      <Text> {nome} </Text>
-      <Text>  Tem {letrasNome} letras. </Text>
-
-      <TouchableOpacity onPress={novoNome}>
-        <Text>Novo nome</Text>
-      </TouchableOpacity>
-
-    </View>
+      <View style={styles.areaBtn}>
+        <TouchableOpacity style={[styles.botao, {backgroundColor: '#1D75CD'}]} onPress={buscar}>
+          <Text style={styles.botaoText}> Buscar </Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={[styles.botao, {backgroundColor: '#CD3E1D'}]} onPress={limpar}>
+          <Text style={styles.botaoText}> Limpar </Text>
+        </TouchableOpacity>
+      </View>
+      {cepUser &&
+      <View style={styles.resultado}>
+        <Text style={styles.itemText}>CEP: {cepUser.cep}</Text>
+        <Text style={styles.itemText}>Logradouro: {cepUser.logradouro}</Text>
+        <Text style={styles.itemText}>Bairro: {cepUser.bairro}</Text>
+        <Text style={styles.itemText}>Cidade: {cepUser.localidade}</Text>
+        <Text style={styles.itemText}>Estado: {cepUser.uf}</Text>
+      </View>
+      }
+    </SafeAreaView>
   ) 
 }
 
 const styles = StyleSheet.create({
   container:{
     flex: 1,
-    marginTop: 20
-  },
-  btn:{
-    backgroundColor: '#222',
+    justifyContent: 'center',
     alignItems: 'center'
   },
-  btnText:{
-    color: '#fff'
+  text:{
+    marginTop: 25,
+    marginBottom: 15,
+    fontSize: 25,
+    fontWeight: 'bold'
+  },
+  input:{
+    backgroundColor: '#FFF',
+    borderWidth: 1,
+    borderColor: '#DDD',
+    borderRadius: 5,
+    width: '90%',
+    padding: 10,
+    fontSize: 18
+  },
+  areaBtn:{
+    alignItems: 'center',
+    flexDirection: 'row',
+    marginTop: 15,
+    justifyContent: 'space-around'
+  },
+  botao:{
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 15,
+    borderRadius: 5,
+  },
+  botaoText:{
+    fontSize: 22,
+    color: '#FFF'
+  },
+  resultado:{
+    flex: 1,
+    justifyContent:'center',
+    alignItems: 'center'
+  },
+  itemText:{
+    fontSize: 22,
+
   }
 })
 export default App;
--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 
 const api = axios.create({
-  baseURL: 'https://free.currencyconverterapi.com/api/v7/'
+  baseURL: 'https://viacep.com/ws'
 })
 
 export default api
