04512fae599bae4681ea3fbdcc434b803cfb2432
GabrielAlvesDeOliveira <alvesg2003@gmail.com>
2022-03-25 10:57:14 -0300

    TarefasApp - Estilizando nossa lista

--- src/components/TaskList/index.js
+++ src/components/TaskList/index.js
@@ -1,10 +1,31 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, TouchableOpacity, TouchableWithoutFeedback } from 'react-native'
+import { Feather } from '@expo/vector-icons'
 
-export default function TaskList({data}) {
+export default function TaskList({data, deleteItem, editItem}) {
   return (
     <View style={styles.container}>
-      <Text>{data.key} {data.name}</Text>
+      <TouchableOpacity style={{marginRight:10}} onPress={()=> deleteItem(data.key)}>
+        <Feather name='trash' size={20} color='#FFF' />
+      </TouchableOpacity>
+
+      <View style={{paddingRight: 10}}>
+        <TouchableWithoutFeedback onPress={() => editItem(data)}>
+          <Text style={{color: '#fff', paddingRight: 10}}>{data.name}</Text>
+        </TouchableWithoutFeedback>
+      </View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#121212',
+    padding: 10,
+    marginBottom: 10,
+    borderRadius: 4,
+  }
+})
