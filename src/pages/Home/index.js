6bdcd8872c7a6cf0287793d26a6f183eeb2afe2c
Gabriel Alves <alvesg2003@gmail.com>
2022-04-24 15:32:08 -0300

    criando home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,15 +2,22 @@ import react, {useContext} from "react";
 import { View, Text, StyleSheet, Button} from 'react-native'
 
 import { AuthContext } from "../../contexts/auth";
+import Header from "../../components/Header";
+
+import { Background, Container, Nome, Saldo, Title } from './styles'
 
 export default function Home(){
 
-  const { user, signOut } = useContext(AuthContext)
+  const { user } = useContext(AuthContext)
   return(
-    <View>
-      <Text> Home </Text>
-      <Text>{ user && user.nome}</Text>
-      <Button title="Sair da conta" onPress={()=> signOut()}/>
-    </View>
+    <Background>
+      <Header/>
+      <Container>
+        <Nome>Gabriel</Nome>
+        <Saldo> R$ 123,00</Saldo>
+      </Container>
+
+      <Title>Ultimas movimentações</Title>
+    </Background>
   )
 }
