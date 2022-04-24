72574e67a384bd6845212137c0b08a6260c25e8a
Gabriel Alves <alvesg2003@gmail.com>
2022-04-24 16:02:56 -0300

    criando home pt2

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,13 +1,19 @@
-import react, {useContext} from "react";
-import { View, Text, StyleSheet, Button} from 'react-native'
+import React, {useContext,useState} from "react";
 
 import { AuthContext } from "../../contexts/auth";
 import Header from "../../components/Header";
+import HistoricoList from "../../components/HistoricoList";
 
 import { Background, Container, Nome, Saldo, Title } from './styles'
 
 export default function Home(){
 
+  const [historico, setHistorico] = useState([
+    {key: '1', tipo: 'receita', valor: 1200}
+    {key: '2', tipo: 'despesa', valor: 200}
+    {key: '3', tipo: 'receita', valor: 40}
+    {key: '4', tipo: 'receita', valor: 89.62}
+  ])
   const { user } = useContext(AuthContext)
   return(
     <Background>
@@ -18,6 +24,13 @@ export default function Home(){
       </Container>
 
       <Title>Ultimas movimentações</Title>
+
+      <List
+      showVerticalScrollIndicator={false}
+      data={historico}
+      keyExtractor={item => item.key}
+      renderItem={ ({item}) => ( <HistoricoList data={item}/>)}
+       />
     </Background>
   )
 }
