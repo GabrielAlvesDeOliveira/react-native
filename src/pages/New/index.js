76a8fb64a404a3bd6d566cce1d22e8408ab1f363
GabrielAlvesDeOliveira <alvesg2003@gmail.com>
2022-04-26 18:55:30 -0300

    registrando um gasto

--- src/pages/New/index.js
+++ src/pages/New/index.js
@@ -1,14 +1,42 @@
 import React, {useState} from 'react';
-import { SafeAreaView, Keyboard, TouchableWithoutFeedback} from 'react-native';
+import { SafeAreaView, Keyboard, TouchableWithoutFeedback, Alert} from 'react-native';
 
 import Header from '../../components/Header';
-import { Background, Input, SubmitButton, subimitText } from './styles';
+import { Background, Input, SubmitButton, SubmitText } from './styles';
 import Picker from '../../components/Picker';
 
 export default function New() {
 
     const [valor, setValor] = useState('')
     const [tipo, setTipo] = useState('receita')
+
+    function handleSubmit(){
+        Keyboard.dismiss()
+        if(isNaN(parseFloat(valor)) || tipo === null){
+            alert('Preencha todos os campos')
+            return
+        }
+
+        Alert.alert(
+            'Confirmando dados',
+            `Tipo ${tipo} - Valor ${parseFloat(valor)}`,
+            [
+                {
+                    text: 'Cancelar',
+                    style: 'cancel'
+                },
+                {
+                    text: 'Continuar',
+                    onPress: () => handleAdd()
+                }
+            ]
+        )
+
+    }
+
+    function handleAdd(){
+        alert("Adicionou")
+    }
 
     return(
         <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>      
@@ -27,8 +55,8 @@ export default function New() {
 
                 <Picker onChange={setTipo} tipo={tipo}/>
 
-                <SubmitButton>
-                    <subimitText>Registrar</subimitText>
+                <SubmitButton onPress={handleSubmit}>
+                    <SubmitText>Registrar</SubmitText>
                 </SubmitButton>
             </SafeAreaView>
         </Background>
