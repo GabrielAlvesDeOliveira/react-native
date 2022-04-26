a012fbcc97c5286af82fba3ab1021a13e1a9f126
GabrielAlvesDeOliveira <alvesg2003@gmail.com>
2022-04-26 18:46:01 -0300

    Criando Tela de Registros

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -9,17 +9,17 @@ import { Background, Container, Nome, Saldo, Title } from './styles'
 export default function Home(){
 
   const [historico, setHistorico] = useState([
-    {key: '1', tipo: 'receita', valor: 1200}
-    {key: '2', tipo: 'despesa', valor: 200}
-    {key: '3', tipo: 'receita', valor: 40}
-    {key: '4', tipo: 'receita', valor: 89.62}
+    {key: '1', tipo: 'receita', valor: 1200},
+    {key: '2', tipo: 'despesa', valor: 200},
+    {key: '3', tipo: 'receita', valor: 40},
+    {key: '4', tipo: 'receita', valor: 89.62},
   ])
   const { user } = useContext(AuthContext)
   return(
     <Background>
       <Header/>
       <Container>
-        <Nome>Gabriel</Nome>
+        <Nome>{user && user.nome}</Nome>
         <Saldo> R$ 123,00</Saldo>
       </Container>
 
