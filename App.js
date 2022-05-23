9c2c99a90e08baa35504b8c05fc19ff8269ac835
GabrielAlvesDeOliveira <alvesg2003@gmail.com>
2022-05-23 17:08:34 -0300

    working with maps

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import MapView from 'react-native-maps';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
 
 export default function App() {
+
+  const [region, setRegion] = useState({
+    latitude: -23.5489,
+    longitude: -46.6388,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  })
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* <Text>Ola mundo</Text> */}
+      <MapView
+        region={region}
+        style={styles.map} />
     </View>
   );
 }
@@ -16,5 +27,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
   },
 });
