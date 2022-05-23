a56d23941c7e2f857bdd0cead26a85dff59c64b8
GabrielAlvesDeOliveira <alvesg2003@gmail.com>
2022-05-23 17:26:07 -0300

    Trabalhando com diferentes localizações

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import MapView from 'react-native-maps';
-import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
 
@@ -11,9 +11,22 @@ export default function App() {
     longitudeDelta: 0.0421,
   })
 
+  function moverCidade(lat, long){
+    setRegion({
+      latitude: lat,
+      longitude: long,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421,
+    })
+  }
+
   return (
     <View style={styles.container}>
-      {/* <Text>Ola mundo</Text> */}
+      <View style={{flexDirection: 'row'}}>
+        <Button title='Brasilia' onPress={()=>{ moverCidade(-15.8080354, -47.8750231) }}/>
+        <Button title='São Paulo' onPress={()=>{ moverCidade(-23.5492243, -46.5813785) }}/>
+      </View>
+      <Text>{region.latitude} | {region.longitude}</Text>
       <MapView
         region={region}
         style={styles.map} />
@@ -29,7 +42,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   map: {
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height,
+    width: '100%',
+    height: 550,
   },
 });
