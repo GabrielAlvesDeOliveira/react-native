9dbd03222310ae56e34b9b56f0709327fd788893
GabrielAlvesDeOliveira <alvesg2003@gmail.com>
2022-06-01 16:48:00 -0300

    Gerador Mega sena

--- src/components/mega/Mega.js
+++ src/components/mega/Mega.js
@@ -1,37 +1,69 @@
 import React, { Component } from 'react';
 import { View, StyleSheet, Text, TextInput, TouchableOpacity } from 'react-native';
-
+import MegaNumero from './Numero';
 
 export default class Mega extends Component {
 
-  
-  constructor(props){
+
+  constructor(props) {
     super(props)
 
     state = {
-      numeros: props.numeros
+      numeros: props.numeros,
+      gerados: [],
     }
 
     this.alterarQtd = this.alterarQtd.bind(this)
   }
 
-  alterarQtd(qtd){
-    this.setState({numeros: qtd})
+  alterarQtd(qtd) {
+    this.setState({ numeros: +qtd })
+  }
+
+  gerarNumeroNaoContido(nums) {
+    const aleatorio = parseInt(Math.random() * 60 + 1)
+    return nums.include(aleatorio) ? this.gerarNumeroNaoContido(nums) : aleatorio
+  }
+
+  gerarNumeros() {
+    const numerics = Array(this.state.qtdNumeros).fill().reduce(nums => [...nums, this.gerarNumeroNaoContido(nums)], [])
+    this.setState({ gerarNumeros: numerics })
+  }
+
+  exibirNumeros() {
+    const nums = this.state.numeros
+    return nums.map(num => {
+      return <MegaNumero num={num} />
+    })
   }
 
   render() {
     return (
-      <Text>
-        {this.state.numeros}
-        Gerador de Mega sena
-        <TextInput 
+      <>
+
+        <Text>
+          Gerador de Mega sena
+        </Text>
+        <TextInput
           keyboardType={'numeric'}
-          style={{borderBottomWidth: 1}}
+          style={{ borderBottomWidth: 1 }}
           placeholder='Quantidade de números'
-          value={this.state.numeros}
+          value={`${this.state.numeros}`}
           onChangeText={qtd => this.alterarQtd(qtd)}
         />
-      </Text>
+        <Button
+          title='Gerar'
+          onPress={this.gerarNumeros}
+        />
+        <View style={{
+          flexDirection: 'row',
+          flexWrap: 'wrap',
+          justifyContent: 'center'
+        }}>
+          {this.exibirNumeros}
+        </View>
+
+      </>
     )
   }
 }
